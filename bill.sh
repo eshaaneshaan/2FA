73445b0e390fe78f1fddbde10da96d5787d564ad
eshaan <92547104+deshnd@users.noreply.github.com>
2024-04-07 23:32:18 -0500

    Update bill.sh

--- bill.sh
+++ bill.sh
@@ -8,9 +8,9 @@ echo "$random_number" >> /home/eshaan-deshmukh/out
 
 while true; do
     if [[ "$1" != "1" ]]; then
-        read -p "You owe me $1 dollars. This screen will not go away until you pay. " input
+        read -p "You owe me $1 dollars for $2. This screen will not go away until you pay. " input
     else
-        read -p "You owe me $1 dollar. This screen will not go away until you pay. " input
+        read -p "You owe me $1 dollar for $2. This screen will not go away until you pay. " input
     fi
     
     # Check if the input matches the random number
