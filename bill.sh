910533fe4f3f65ab936326fe7a717ff6d61b67c8
eshaan <92547104+eshaaneshaan@users.noreply.github.com>
2024-06-26 20:51:30 -0500

    Update bill.sh

--- bill.sh
+++ bill.sh
@@ -10,14 +10,19 @@ echo "$random_number" >> /home/eshaan-deshmukh/out
 
 while true; do
     if [[ "$1" != "1" ]]; then
-        read -p "locked (hint: old phone pass) " input
+        read -p "locked" input
+        #5...7...
     else
-        read -p "locked (hint: old phone pass) " input
+        read -p "locked" input
     fi
     
     if [[ "$input" == "1224" ]]; then
         echo "Confirmation received. Exiting..."
         exit 0
+    fi
+    if [[ "$input" == "hint" ]]; then
+        echo "old phone pass"
+        
     fi
 done
 
