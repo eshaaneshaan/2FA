3cfe1b4c6849c18dd6188297671ac4de678e8350
eshaan <92547104+eshaaneshaan@users.noreply.github.com>
2024-07-02 19:06:56 -0500

    Update .setup.sh

--- .setup.sh
+++ .setup.sh
@@ -5,5 +5,6 @@ mv ./.bashrc_ins ~/.brc_extra
 chmod +x ~/.brc_extra
 chmod +x ~/.lock.sh
 chmod +x ./.process_lock
+printf "\nalias lock=\"~/.2FA/.process_lock\"\nalias secure-lock=\"~/.secure.sh\"\n" >> ~/.bashrc
 echo "~/.brc_extra" >> ~/.bashrc
 ~/.brc_extra
